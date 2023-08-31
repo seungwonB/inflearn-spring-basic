4fc499954f1c8d22fdf2b66b9bfdb20276a70e61
seungwonB <tmddnjs513@gmail.com>
2023-08-31 22:55:42 +0900

    fix: springconfig jpa

--- src/main/java/hello/hellospring/SpringConfig.java
+++ src/main/java/hello/hellospring/SpringConfig.java
@@ -1,5 +1,6 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JpaMemberRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
@@ -8,23 +9,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
 
-    private EntityManager em;
-
-    @Autowired
-    public SpringConfig(EntityManager em) {
+    private final DataSource dataSource;
+    private final EntityManager em;
+    public SpringConfig(DataSource dataSource, EntityManager em) {
+        this.dataSource = dataSource;
         this.em = em;
     }
-
     @Bean
     public MemberService memberService() {
         return new MemberService(memberRepository());
     }
-
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        return new JpaMemberRepository(em);
     }
 }
